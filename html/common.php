b05ff84b4c03659a4d3508098de642f4e8f5f54d
gtheler <jeremy@seamplex.com>
2025-12-26 14:20:55 -0300

    fix for log_error

--- html/common.php
+++ html/common.php
@@ -39,9 +39,7 @@ function suncae_log_error($message) {
 
   $date = date('Y-m-d');
   suncae_log_write("{$log_dir}error.log", $username, $message);
-  if ($level > 0) {
-    suncae_log_write("{$log_dir}{$level}-{$date}.log", $username, $message);
-  }
+  suncae_log_write("{$log_dir}0-{$date}.log", $username, $message);
 }
 
 
